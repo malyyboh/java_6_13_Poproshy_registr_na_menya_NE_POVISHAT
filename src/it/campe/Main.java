7c0cbae26c2c0506d42a6c7dfa4a05bc497dcd94
Ksenx73 <hello@itcampe.com>
2021-10-14 17:09:01 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,14 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+
+        //напишите тут ваш код
+        System.out.println(usa);
+        System.out.println("Винни Пух");
+        System.out.println(caps + "they know I mean business");
+    }
+}
